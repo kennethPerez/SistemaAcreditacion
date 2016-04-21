7882003b092f4f89feae28068926b7f092c2e241
Kenneth Perez <kenn.perez@gmail.com>
2016-04-20 18:13:09 -0600

    Porlet Info

--- scripts/controllers/cordinatorController.js
+++ scripts/controllers/cordinatorController.js
@@ -723,8 +723,17 @@ angular.module("app")
     /*************************************************************************************************/
 
 
-    $scope.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    $scope.actividades = [{}]
+    $scope.text = "Conformación de comisión de profesores para atender el enlace con la industria.";
 
+    $scope.actionDisplay = "Mostar más";
 
-
+    $scope.changeAction = function(a){
+        if(a == "Mostar más"){
+            $scope.actionDisplay = "Mostrar menos"
+        }
+        else{
+            $scope.actionDisplay = "Mostar más";
+        }
+    }
 })
